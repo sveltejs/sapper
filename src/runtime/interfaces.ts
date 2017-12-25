b3027c581665671ee43b2f093b62cbe91cdc41c2
Rich Harris <richard.a.harris@gmail.com>
2017-12-25 17:14:39 -0500

    port runtime to typescript, move runtime/app.js to runtime.js

---
+++ src/runtime/interfaces.ts
@@ -0,0 +1,23 @@
+export type Params = Record<string, string>;
+export type Query = Record<string, string | true>;
+export type RouteData = { params: Params, query: Query };
+
+export interface ComponentConstructor {
+	new (options: { target: Node, data: any, hydrate: boolean }): Component;
+	preload: (data: { params: Params, query: Query }) => Promise<any>;
+};
+
+export interface Component {
+	destroy: () => void;
+}
+
+export type Route = {
+	pattern: RegExp;
+	params: (match: RegExpExecArray) => Record<string, string>;
+	load: () => Promise<{ default: ComponentConstructor }>
+};
+
+export type ScrollPosition = {
+	x: number;
+	y: number;
+};
