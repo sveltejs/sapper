e3c047831a0edba9957a6482bd0cea228586dc09
Rich Harris <richard.a.harris@gmail.com>
2018-01-13 23:29:16 -0500

    add test for #77

--- test/common/test.js
+++ test/common/test.js
@@ -289,6 +289,19 @@ function run(env) {
 
 				assert.equal(html, `URL is /show-url`);
 			});
+
+			it('calls a delete handler', async () => {
+				await nightmare
+					.goto(`${base}/delete-test`)
+					.wait(() => window.READY)
+					.click('.del')
+					.wait(() => window.deleted);
+
+				assert.equal(
+					await nightmare.evaluate(() => window.deleted.id),
+					42
+				);
+			});
 		});
 
 		describe('headers', () => {
