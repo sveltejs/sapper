55b60369f953a764dabbf2a3bab72c37de418f5f
Rich Harris <richard.a.harris@gmail.com>
2018-02-13 17:12:55 -0500

    add --help to CLI

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,15 +1,34 @@
+import mri from 'mri';
+import chalk from 'chalk';
+import help from './help.md';
 import build from './build.js';
 import exporter from './export.js';
-import { dest, entry, isDev, src } from '../config';
+import { dest, entry, src } from '../config';
+import * as pkg from '../../package.json';
 
-process.env.NODE_ENV = 'production';
+const opts = mri(process.argv.slice(2), {
+	alias: {
+		h: 'help'
+	}
+});
 
-const cmd = process.argv[2];
+if (opts.help) {
+	const rendered = help
+		.replace('<@version@>', pkg.version)
+		.replace(/^(.+)/gm, (m: string, $1: string) => /[#>]/.test(m) ? $1 : `  ${$1}`)
+		.replace(/^# (.+)/gm, (m: string, $1: string) => chalk.bold.underline($1))
+		.replace(/^> (.+)/gm, (m: string, $1: string) => chalk.cyan($1));
+
+	console.log(`\n${rendered}\n`);
+	process.exit(0);
+}
+
+const [cmd] = opts._;
+
 const start = Date.now();
-const dev = isDev();
 
 if (cmd === 'build') {
-	build({ dest, dev, entry, src })
+	build({ dest, dev: false, entry, src })
 		.then(() => {
 			const elapsed = Date.now() - start;
 			console.error(`built in ${elapsed}ms`); // TODO beautify this, e.g. 'built in 4.7 seconds'
