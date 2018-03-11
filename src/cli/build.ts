ba3d9c85c5aa73a67a76c20bde1d385ce87ae38a
Rich Harris <richard.a.harris@gmail.com>
2018-03-10 23:09:02 -0500

    clorox seems to have a bug with inverse (TODO investigate)

--- src/cli/build.ts
+++ src/cli/build.ts
@@ -20,12 +20,12 @@ export async function build() {
 	const { client, server, serviceworker } = create_compilers();
 
 	const client_stats = await compile(client);
-	console.log(clorox.inverse(`\nbuilt client`));
+	console.log(clorox.inverse(`\nbuilt client`).toString());
 	console.log(client_stats.toString({ colors: true }));
 	fs.writeFileSync(path.join(output, 'client_info.json'), JSON.stringify(client_stats.toJson()));
 
 	const server_stats = await compile(server);
-	console.log(clorox.inverse(`\nbuilt server`));
+	console.log(clorox.inverse(`\nbuilt server`).toString());
 	console.log(server_stats.toString({ colors: true }));
 
 	let serviceworker_stats;
@@ -38,7 +38,7 @@ export async function build() {
 		});
 
 		serviceworker_stats = await compile(serviceworker);
-		console.log(clorox.inverse(`\nbuilt service worker`));
+		console.log(clorox.inverse(`\nbuilt service worker`).toString());
 		console.log(serviceworker_stats.toString({ colors: true }));
 	}
 }
