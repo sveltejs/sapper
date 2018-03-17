9812cbd71ca4087d0ebacad255c94c9b5dc29a85
Rich Harris <richard.a.harris@gmail.com>
2018-03-17 13:45:59 -0400

    add server- and client-side store management (#178)

--- test/app/app/client.js
+++ test/app/app/client.js
@@ -1,8 +1,11 @@
 import { init, prefetchRoutes } from '../../../runtime.js';
+import { Store } from 'svelte/store.js';
 import { routes } from './manifest/client.js';
 
 window.init = () => {
-	return init(document.querySelector('#sapper'), routes);
+	return init(document.querySelector('#sapper'), routes, {
+		store: data => new Store(data)
+	});
 };
 
 window.prefetchRoutes = prefetchRoutes;
