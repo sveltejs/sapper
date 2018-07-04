008b607c015b9bd7e06b40f5f1278c490e68ed8b
Rich Harris <richard.a.harris@gmail.com>
2018-07-04 10:53:41 -0400

    generate pages and server routes separately

--- src/core/create_routes.ts
+++ src/core/create_routes.ts
@@ -3,8 +3,16 @@ import glob from 'glob';
 import { locations } from '../config';
 import { Route } from '../interfaces';
 
-export default function create_routes({ files } = { files: glob.sync('**/*.*', { cwd: locations.routes(), dot: true, nodir: true }) }) {
-	const routes: Route[] = files
+export default function create_routes({
+	files
+} = {
+	files: glob.sync('**/*.*', {
+		cwd: locations.routes(),
+		dot: true,
+		nodir: true
+	})
+}) {
+	const all_routes = files
 		.filter((file: string) => !/(^|\/|\\)(_(?!error\.html)|\.(?!well-known))/.test(file))
 		.map((file: string) => {
 			if (/]\[/.test(file)) {
@@ -22,82 +30,14 @@ export default function create_routes({ files } = { files: glob.sync('**/*.*', {
 				if (parts.length > 0) parts[parts.length - 1] += part.slice(5);
 			}
 
-			return {
-				files: [file],
-				base,
-				parts
-			};
-		})
-		.filter(Boolean)
-		.filter((a, index, array) => {
-			const found = array.slice(index + 1).find(b => a.base === b.base);
-			if (found) found.files.push(...a.files);
-			return !found;
-		})
-		.sort((a, b) => {
-			if (a.parts[0] === '_error') return -1;
-			if (b.parts[0] === '_error') return 1;
-
-			const max = Math.max(a.parts.length, b.parts.length);
-
-			for (let i = 0; i < max; i += 1) {
-				const a_part = a.parts[i];
-				const b_part = b.parts[i];
-
-				if (!a_part) return -1;
-				if (!b_part) return 1;
-
-				const a_sub_parts = get_sub_parts(a_part);
-				const b_sub_parts = get_sub_parts(b_part);
-				const max = Math.max(a_sub_parts.length, b_sub_parts.length);
-
-				for (let i = 0; i < max; i += 1) {
-					const a_sub_part = a_sub_parts[i];
-					const b_sub_part = b_sub_parts[i];
-
-					if (!a_sub_part) return 1; // b is more specific, so goes first
-					if (!b_sub_part) return -1;
-
-					if (a_sub_part.dynamic !== b_sub_part.dynamic) {
-						return a_sub_part.dynamic ? 1 : -1;
-					}
-
-					if (!a_sub_part.dynamic && a_sub_part.content !== b_sub_part.content) {
-						return (
-							(b_sub_part.content.length - a_sub_part.content.length) ||
-							(a_sub_part.content < b_sub_part.content ? -1 : 1)
-						);
-					}
-
-					// If both parts dynamic, check for regexp patterns
-					if (a_sub_part.dynamic && b_sub_part.dynamic) {
-						const regexp_pattern = /\((.*?)\)/;
-						const a_match = regexp_pattern.exec(a_sub_part.content);
-						const b_match = regexp_pattern.exec(b_sub_part.content);
-
-						if (!a_match && b_match) {
-							return 1; // No regexp, so less specific than b
-						}
-						if (!b_match && a_match) {
-							return -1; 
-						}
-						if (a_match && b_match && a_match[1] !== b_match[1]) {
-							return b_match[1].length - a_match[1].length;
-						}
-					}
-				}
-			}
-
-			throw new Error(`The ${a.base} and ${b.base} routes clash`);
-		})
-		.map(({ files, base, parts }) => {
 			const id = (
 				parts.join('_').replace(/[[\]]/g, '$').replace(/^\d/, '_$&').replace(/[^a-zA-Z0-9_$]/g, '_')
-			 ) || '_';
+			) || '_';
 
 			const params: string[] = [];
-			const match_patterns: object = {};
+			const match_patterns: Record<string, string> = {};
 			const param_pattern = /\[([^\(\]]+)(?:\((.+?)\))?\]/g;
+
 			let match;
 			while (match = param_pattern.exec(base)) {
 				params.push(match[1]);
@@ -152,20 +92,9 @@ export default function create_routes({ files } = { files: glob.sync('**/*.*', {
 
 			return {
 				id,
-				handlers: files.map(file => ({
-					type: path.extname(file) === '.html' ? 'page' : 'route',
-					file
-				})).sort((a, b) => {
-					if (a.type === 'page' && b.type === 'route') {
-						return 1;
-					}
-
-					if (a.type === 'route' && b.type === 'page') {
-						return -1;
-					}
-
-					return 0;
-				}),
+				base,
+				type: file.endsWith('.html') ? 'page' : 'route',
+				file,
 				pattern,
 				test,
 				exec,
@@ -174,7 +103,72 @@ export default function create_routes({ files } = { files: glob.sync('**/*.*', {
 			};
 		});
 
-	return routes;
+	const pages = all_routes
+		.filter(r => r.type === 'page')
+		.sort(comparator);
+
+	const server_routes = all_routes
+		.filter(r => r.type === 'route')
+		.sort(comparator);
+
+	return { pages, server_routes };
+}
+
+function comparator(a, b) {
+	if (a.parts[0] === '_error') return -1;
+	if (b.parts[0] === '_error') return 1;
+
+	const max = Math.max(a.parts.length, b.parts.length);
+
+	for (let i = 0; i < max; i += 1) {
+		const a_part = a.parts[i];
+		const b_part = b.parts[i];
+
+		if (!a_part) return -1;
+		if (!b_part) return 1;
+
+		const a_sub_parts = get_sub_parts(a_part);
+		const b_sub_parts = get_sub_parts(b_part);
+		const max = Math.max(a_sub_parts.length, b_sub_parts.length);
+
+		for (let i = 0; i < max; i += 1) {
+			const a_sub_part = a_sub_parts[i];
+			const b_sub_part = b_sub_parts[i];
+
+			if (!a_sub_part) return 1; // b is more specific, so goes first
+			if (!b_sub_part) return -1;
+
+			if (a_sub_part.dynamic !== b_sub_part.dynamic) {
+				return a_sub_part.dynamic ? 1 : -1;
+			}
+
+			if (!a_sub_part.dynamic && a_sub_part.content !== b_sub_part.content) {
+				return (
+					(b_sub_part.content.length - a_sub_part.content.length) ||
+					(a_sub_part.content < b_sub_part.content ? -1 : 1)
+				);
+			}
+
+			// If both parts dynamic, check for regexp patterns
+			if (a_sub_part.dynamic && b_sub_part.dynamic) {
+				const regexp_pattern = /\((.*?)\)/;
+				const a_match = regexp_pattern.exec(a_sub_part.content);
+				const b_match = regexp_pattern.exec(b_sub_part.content);
+
+				if (!a_match && b_match) {
+					return 1; // No regexp, so less specific than b
+				}
+				if (!b_match && a_match) {
+					return -1;
+				}
+				if (a_match && b_match && a_match[1] !== b_match[1]) {
+					return b_match[1].length - a_match[1].length;
+				}
+			}
+		}
+	}
+
+	throw new Error(`The ${a.base} and ${b.base} routes clash`);
 }
 
 function get_sub_parts(part: string) {
