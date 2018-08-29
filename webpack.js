f05a8e52a004a4cb2a31998d36013c8f77303f24
Rich Harris <richard.a.harris@gmail.com>
2018-08-29 18:01:27 -0400

    deprecate, dont break

--- webpack.js
+++ webpack.js
@@ -1,1 +1,2 @@
-throw new Error(`As of Sapper 0.18, webpack config must be loaded from sapper/config/webpack.js`);
+console.error(`[DEPRECATION] As of Sapper 0.18, webpack config should be loaded from sapper/config/webpack.js`);
+module.exports = require('./dist/webpack.js');
