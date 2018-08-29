76cb6d97f3b2862714f46fef016199342b16ed83
Rich Harris <richard.a.harris@gmail.com>
2018-08-29 18:01:17 -0400

    oops

--- src/api/utils/copy_shimport.ts
+++ src/api/utils/copy_shimport.ts
@@ -4,6 +4,6 @@ export function copy_shimport(dest: string) {
 	const shimport_version = require('shimport/package.json').version;
 	fs.writeFileSync(
 		`${dest}/client/shimport@${shimport_version}.js`,
-		fs.readFileSync(require.resolve('shimport/index.dev.js'))
+		fs.readFileSync(require.resolve('shimport/index.js'))
 	);
 }
