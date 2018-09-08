cbb5e8755ba8955061b289afdca9c278ee4be694
Rich Harris <richard.a.harris@gmail.com>
2018-09-08 10:12:55 -0400

    Use MDN recommendation for preventing SW caching

--- src/middleware.ts
+++ src/middleware.ts
@@ -141,12 +141,12 @@ export default function middleware(opts: {
 
 		fs.existsSync(path.join(output, 'service-worker.js')) && serve({
 			pathname: '/service-worker.js',
-			cache_control: 'max-age=0'
+			cache_control: 'no-cache, no-store, must-revalidate'
 		}),
 
 		fs.existsSync(path.join(output, 'service-worker.js.map')) && serve({
 			pathname: '/service-worker.js.map',
-			cache_control: 'max-age=0'
+			cache_control: 'no-cache, no-store, must-revalidate'
 		}),
 
 		serve({
