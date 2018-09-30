2e2b8dcd83daaafa00bc50ba3e8a11767230b07a
Rich Harris <richard.a.harris@gmail.com>
2018-09-30 18:26:44 -0400

    small tweak

--- templates/src/client/goto/index.ts
+++ templates/src/client/goto/index.ts
@@ -1,16 +1,13 @@
-import { select_route, navigate, cid } from '../app';
+import { history, select_route, navigate, cid } from '../app';
 
 export default function goto(href: string, opts = { replaceState: false }) {
 	const target = select_route(new URL(href, document.baseURI));
-	let promise;
 
 	if (target) {
-		promise = navigate(target, null).then(() => {});
-		if (history) history[opts.replaceState ? 'replaceState' : 'pushState']({ id: cid }, '', href);
-	} else {
-		location.href = href;
-		promise = new Promise(f => {}); // never resolves
+		history[opts.replaceState ? 'replaceState' : 'pushState']({ id: cid }, '', href);
+		return navigate(target, null).then(() => {});
 	}
 
-	return promise;
+	location.href = href;
+	return new Promise(f => {}); // never resolves
 }
