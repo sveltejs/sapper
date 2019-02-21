554c3f263645198bf00fed7fbbee115ca6f46bbf
Richard Harris <richard.a.harris@gmail.com>
2019-02-20 20:55:32 -0500

    rename create_main_manifests to create_app

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,3 @@
-export * from './core/create_manifests';
+export * from './core/create_app';
 export { default as create_compilers } from './core/create_compilers/index';
 export { default as create_manifest_data } from './core/create_manifest_data';
