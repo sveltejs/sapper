6d5aa9a35dd92d2853141ccc329d9a4db6bca620
Conduitry <git@chor.date>
2019-03-11 10:39:35 -0400

    fix handling of empty hrefs during export (#602)

--- test/apps/export/src/routes/index.svelte
+++ test/apps/export/src/routes/index.svelte
@@ -2,3 +2,8 @@
 
 <a href="blog">blog</a>
 <a href="">empty anchor</a>
+<a href=''>empty anchor #2</a>
+<a href=>empty anchor #3</a>
+<a href= >empty anchor #4</a>
+<a href>empty anchor #5</a>
+<a>empty anchor #6</a>
