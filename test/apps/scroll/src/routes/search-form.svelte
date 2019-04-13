2863219747481afd254a8d4b5f1c7527c6321725
Christophe Benz <christophe.benz@gmail.com>
2019-04-13 13:54:06 +0200

    Add test

---
+++ test/apps/scroll/src/routes/search-form.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { goto } from "@sapper/app";
+
+	function handleSearch() {
+		goto("?q=foo");
+	}
+</script>
+
+<h1>A search form</h1>
+
+<div style="height: 9999px"></div>
+
+<div id="search">
+	<button on:click={handleSearch()}>Search</button>
+</div>
