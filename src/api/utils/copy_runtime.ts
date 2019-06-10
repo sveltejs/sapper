8baf694e6d80d4ebe2ecdc705397ee19be85d490
Richard Harris <richard.a.harris@gmail.com>
2019-06-09 20:10:56 -0400

    revert previous unpleasantness

--- src/api/utils/copy_runtime.ts
+++ src/api/utils/copy_runtime.ts
@@ -1,13 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import relative from 'require-relative';
 import { mkdirp } from './fs_utils';
-
-const svelte_pkg = relative('svelte/package.json', process.cwd());
-const match = /(\d+)\.(\d+)\.(\d+)/.exec(svelte_pkg.version);
-const legacy = match
-	? ((+match[1] - 3) || (+match[2] - 4) || (+match[3] - 4)) <= 0
-	: false; // ???
 
 const runtime = [
 	'app.mjs',
@@ -23,8 +16,6 @@ const runtime = [
 export function copy_runtime(output: string) {
 	runtime.forEach(({ file, source }) => {
 		mkdirp(path.dirname(`${output}/${file}`));
-
-		if (!legacy) source = source.replace(/svelte\/(.+)\.mjs/g, `svelte/$1/index.mjs`);
 		fs.writeFileSync(`${output}/${file}`, source);
 	});
 }
