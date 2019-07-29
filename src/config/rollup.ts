c653eef818ae24242a582e9dd2135dba434df16f
Teodor Tanasoaia <28601907+Teoxoy@users.noreply.github.com>
2019-07-29 21:33:59 +0200

    rollup sourcemap adjustments

--- src/config/rollup.ts
+++ src/config/rollup.ts
@@ -1,14 +1,17 @@
 import { dev, src, dest } from './env';
+import { InputOption, OutputOptions } from 'rollup';
+
+const sourcemap = dev ? 'inline' : false;
 
 export default {
 	dev,
 
 	client: {
-		input: () => {
+		input: (): InputOption => {
 			return `${src}/client.js`
 		},
 
-		output: () => {
+		output: (): OutputOptions => {
 			let dir = `${dest}/client`;
 			if (process.env.SAPPER_LEGACY_BUILD) dir += `/legacy`;
 
@@ -17,36 +20,37 @@ export default {
 				entryFileNames: '[name].[hash].js',
 				chunkFileNames: '[name].[hash].js',
 				format: 'esm',
-				sourcemap: dev
+				sourcemap
 			};
 		}
 	},
 
 	server: {
-		input: () => {
+		input: (): InputOption => {
 			return {
 				server: `${src}/server.js`
 			};
 		},
 
-		output: () => {
+		output: (): OutputOptions => {
 			return {
 				dir: `${dest}/server`,
 				format: 'cjs',
-				sourcemap: dev
+				sourcemap
 			};
 		}
 	},
 
 	serviceworker: {
-		input: () => {
+		input: (): InputOption => {
 			return `${src}/service-worker.js`;
 		},
 
-		output: () => {
+		output: (): OutputOptions => {
 			return {
 				file: `${dest}/service-worker.js`,
-				format: 'iife'
+				format: 'iife',
+				sourcemap
 			}
 		}
 	}
