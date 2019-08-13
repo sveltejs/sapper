ef88116fb479dc1f4c62c26d15f001259cf48013
Sander Hahn <sanderhahn@gmail.com>
2019-08-13 02:01:21 +0200

    refresh manifest_data has_preload on changes

--- src/api/dev.ts
+++ src/api/dev.ts
@@ -514,7 +514,8 @@ function watch_dir(
 		watch = new CheapWatch({ dir, filter, debounce: 50 });
 
 		watch.on('+', ({ isNew }: { isNew: boolean }) => {
-			if (isNew) callback();
+			// ignore isNew to refresh the manifest_data.components has_preload values
+			callback();
 		});
 
 		watch.on('-', callback);
