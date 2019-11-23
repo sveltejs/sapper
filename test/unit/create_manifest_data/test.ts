01ba1a7473d4a9722ec1150756928448f3ea9ccc
John Peel <john@dgby.org>
2019-11-23 14:27:22 -0500

    Allow multiple slugs in server route filename
    Regex for getting parts from a file segement was made non-greedy.
    Added regex sanitizing to non-dynamic parts of segment.
    Added a test for the new ability for multiple slugs.

--- test/unit/create_manifest_data/test.ts
+++ test/unit/create_manifest_data/test.ts
@@ -59,14 +59,14 @@ describe('manifest_data', () => {
 
 			{
 				name: 'route_blog_json',
-				pattern: /^\/blog.json$/,
+				pattern: /^\/blog\.json$/,
 				file: 'blog/index.json.js',
 				params: []
 			},
 
 			{
 				name: 'route_blog_$slug_json',
-				pattern: /^\/blog\/([^\/]+?).json$/,
+				pattern: /^\/blog\/([^\/]+?)\.json$/,
 				file: 'blog/[slug].json.js',
 				params: ['slug']
 			}
@@ -140,6 +140,19 @@ describe('manifest_data', () => {
 
 		assert.deepEqual(server_routes.map(r => r.file), [
 			'.well-known/dnt-policy.txt.js'
+		]);
+	});
+
+	it('allows mutliple slugs', () => {
+		const { server_routes } = create_manifest_data(path.join(__dirname, 'samples/multiple-slugs'));
+
+		assert.deepEqual(server_routes, [
+			{
+				name: "route_$file$93_$91ext",
+				pattern: /^\/([^\/]+?)\.([^\/]+?)$/,
+				file: "[file].[ext].js",
+				params: ["file", "ext"]
+			}
 		]);
 	});
 
@@ -226,13 +239,13 @@ describe('manifest_data', () => {
 		assert.deepEqual(server_routes, [
 			{
 				name: 'route_blog_json',
-				pattern: /^\/blog.json$/,
+				pattern: /^\/blog\.json$/,
 				file: 'blog/index.json.js',
 				params: []
 			},
 			{
 				name: 'route_blog_$slug_json',
-				pattern: /^\/blog\/([^\/]+?).json$/,
+				pattern: /^\/blog\/([^\/]+?)\.json$/,
 				file: 'blog/[slug].json.js',
 				params: ['slug']
 			}
