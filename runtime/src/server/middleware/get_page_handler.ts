236c596d72c77fe7482e3c7ea0dfc60b91edf6f2
Martin Burger <m@rtin-burger.net>
2020-06-02 20:32:51 +0200

    Make Content Security Policy (CSP) nonce available in template.html

--- runtime/src/server/middleware/get_page_handler.ts
+++ runtime/src/server/middleware/get_page_handler.ts
@@ -321,13 +321,15 @@ export function get_page_handler(
 
 			// users can set a CSP nonce using res.locals.nonce
 			const nonce_attr = (res.locals && res.locals.nonce) ? ` nonce="${res.locals.nonce}"` : '';
+			const nonce_value = (res.locals && res.locals.nonce) ? res.locals.nonce : '';
 
 			const body = template()
 				.replace('%sapper.base%', () => `<base href="${req.baseUrl}/">`)
 				.replace('%sapper.scripts%', () => `<script${nonce_attr}>${script}</script>`)
 				.replace('%sapper.html%', () => html)
 				.replace('%sapper.head%', () => `<noscript id='sapper-head-start'></noscript>${head}<noscript id='sapper-head-end'></noscript>`)
-				.replace('%sapper.styles%', () => styles);
+				.replace('%sapper.styles%', () => styles)
+				.replace('%sapper.cspnonce%', () => nonce_value);
 
 			res.statusCode = status;
 			res.end(body);
