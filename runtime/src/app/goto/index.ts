d6a9f14609f5b15ae955824ed87740b466cd0467
Daniele Loreto <53383860+monkeythedev@users.noreply.github.com>
2020-07-26 22:40:25 +0200

    spaces replaced with tabs

--- runtime/src/app/goto/index.ts
+++ runtime/src/app/goto/index.ts
@@ -4,7 +4,7 @@ export default function goto(href: string, opts = { replaceState: false, state: 
 	const target = select_target(new URL(href, document.baseURI));
 
 	if (target) {
-                const state = opts.state || {};
+		const state = opts.state || {};
 		history[opts.replaceState ? 'replaceState' : 'pushState']({...state, id: cid }, '', href);
 		return navigate(target, null).then(() => {});
 	}
