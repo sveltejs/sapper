00304633b61ce6a3f7f9366333a45cd2d7c1d300
Daniele Loreto <53383860+monkeythedev@users.noreply.github.com>
2020-07-26 22:31:24 +0200

    Making a non breaking change

--- runtime/src/app/goto/index.ts
+++ runtime/src/app/goto/index.ts
@@ -1,9 +1,10 @@
 import { history, select_target, navigate, cid } from '../app';
 
-export default function goto(href: string, state = {}, opts = { replaceState: false }) {
+export default function goto(href: string, opts = { replaceState: false, state: {} }) {
 	const target = select_target(new URL(href, document.baseURI));
 
 	if (target) {
+                const state = opts.state || {};
 		history[opts.replaceState ? 'replaceState' : 'pushState']({...state, id: cid }, '', href);
 		return navigate(target, null).then(() => {});
 	}
