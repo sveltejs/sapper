83b3f79af0e43fec1807c4a3b1d1b2ed6e8b3a82
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-07-30 09:45:40 -0700

    Update old link to migration guide (#1349)

--- site/scripts/deploy.sh
+++ site/scripts/deploy.sh
@@ -4,7 +4,8 @@ git symbolic-ref HEAD refs/heads/gh-pages
 git reset $HEAD
 git rm --cached -rf ..
 cd __sapper__/export
-find . -type f | cut -c 3- | xargs -I '{}' sh -c 'git update-index --add --cacheinfo 100644,$(git hash-object -w "{}"),"{}"'
+files=($(find -type f | cut -c 3-))
+for file in ${files[@]}; do hash=$(git hash-object -w $file); git update-index --add --cacheinfo 100644,$hash,$file; done
 git commit -m '[build site]'
 git symbolic-ref HEAD $HEAD
 git reset
