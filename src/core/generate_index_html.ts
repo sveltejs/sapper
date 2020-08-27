18be869976c3a06403695277992cf53483a9f03c
dishuostec <dishuostec@gmail.com>
2020-08-27 11:27:48 +0800

    remove styles process

--- src/core/generate_index_html.ts
+++ src/core/generate_index_html.ts
@@ -64,18 +64,6 @@ export function create_index_html({
 		script += `</script><script src="${main}">`;
 	}
 
-	let styles: string;
-
-	if (build_info.css && build_info.css.main) {
-		const css_chunks = new Set();
-		if (build_info.css.main) css_chunks.add(build_info.css.main);
-
-		styles = Array.from(css_chunks)
-			.map(href => `<link rel="stylesheet" href="client/${href}">`)
-			.join('');
-	} else {
-		styles = '';
-	}
 
 	const body = template()
 		.replace('%sapper.base%', () => `<base href="${basepath}/">`)
@@ -83,7 +71,7 @@ export function create_index_html({
 		.replace('%sapper.TIMESTAMP%', () => process.env.TIMESTAMP || Date.now().toString())
 		.replace('%sapper.html%', () => '')
 		.replace('%sapper.head%', () => '')
-		.replace('%sapper.styles%', () => styles);
+		.replace('%sapper.styles%', () => '');
 
 
 	write_if_changed(`${build_dir}/index.html`, body);
