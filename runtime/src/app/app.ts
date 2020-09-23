dd5fd848c4a9e805250598505da568e8ab2d8aaf
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2020-09-23 15:55:21 +0200

    trigger new build

--- runtime/src/app/app.ts
+++ runtime/src/app/app.ts
@@ -2,5 +2,5 @@
 export { default as stores } from './stores';
 export { default as start } from './start';
 export { default as goto } from './goto';
-export { default as prefetch } from './prefetch';
 export { default as prefetchRoutes } from './prefetchRoutes';
+export { default as prefetch } from './prefetch';
