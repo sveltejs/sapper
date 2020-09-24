b9feaeb98c5979a4ea5d01d3f7ecc8463efe60e7
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2020-09-24 16:36:56 +0200

    Eliminating the rollup warning "'preload' is not exported"

--- runtime/src/app/app.ts
+++ runtime/src/app/app.ts
@@ -209,7 +209,7 @@ export async function hydrate_target(dest: Target): Promise<HydratedTarget> {
 	};
 
 	if (!root_preloaded) {
-		const root_preload = root_comp.preload || (() => ({}));
+		const { preload: root_preload = (() => ({})) } = root_comp;
 		root_preloaded = initial_data.preloaded[0] || root_preload.call(preload_context, {
 			host: page.host,
 			path: page.path,
