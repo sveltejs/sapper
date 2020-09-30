98e6d3293d0f89e63476d1c2fd362cfadf091590
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2020-09-30 15:42:29 +0200

    relying more heavily on svelte common rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
 	root: true,
 	extends: [
-		'@sveltejs',
-		'eslint:recommended',
-		'plugin:@typescript-eslint/eslint-recommended',
-		'plugin:@typescript-eslint/recommended'
+		'@sveltejs'
 	],
 	plugins: ['import'],
 	settings: {
@@ -15,11 +12,6 @@ module.exports = {
 	rules: {
 		indent: ['error', 'tab'],
 		'brace-style': ['error', '1tbs'],
-		'@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
-		'@typescript-eslint/ban-types': 'off',
-		'@typescript-eslint/no-var-requires': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-empty-function': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off'
+		'@typescript-eslint/no-unused-vars': ['error', { args: 'none' }]
 	}
 };
