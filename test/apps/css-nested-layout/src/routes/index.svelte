b3d42c360330a365fa2148c6a9073becbc878cd4
Ronan Doherty <ronan.odochartaigh@gmail.com>
2020-10-02 20:19:06 +0100

    Revert "Revert "Fix #1579""
    This reverts commit f2cb6f44bd84e6a7881abc20aa1795275d342d4c.

---
+++ test/apps/css-nested-layout/src/routes/index.svelte
@@ -0,0 +1,7 @@
+<h1>Great success!</h1>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
---
+++ test/apps/css-nested-layout/src/routes/nested/_layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Test from '../_components/Test.svelte';
+</script>
+
+<h2>This is content defined in a layout in a nested route. It should have a green background.</h2>
+
+<Test/>
+
+<slot></slot>
+
+<style>
+    h2 {
+        background-color: rgb(0, 128, 0);
+        color: #fff;
+        margin-bottom: 2rem;
+    }
+</style>
