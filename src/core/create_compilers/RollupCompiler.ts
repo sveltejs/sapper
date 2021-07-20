ae00b6ac425620139659711b426bec5b7a4df39a
Ankit <ankitstarski@gmail.com>
2021-07-21 00:44:24 +0530

    replace onload with addEventListener in inject_styles (#1790)

--- src/core/create_compilers/RollupCompiler.ts
+++ src/core/create_compilers/RollupCompiler.ts
@@ -50,8 +50,8 @@ export default function(files) {
 		if (link.sheet) {
 			fulfil();
 		} else {
-			link.onload = function() { return fulfil() };
-			link.onerror = reject;
+			link.addEventListener('load', function() { return fulfil() });
+			link.addEventListener('error', reject);
 		}
 	})}));
 };`.trim();
